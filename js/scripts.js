b6838961ad42fb2bfdb2862c273ea468f16623be
Alisa Logacheva <logacheva.alisa@gmail.com>
2024-05-20 18:32:54 -0400

    Control commit

--- js/scripts.js
+++ js/scripts.js
@@ -49,34 +49,6 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-
-// Reviews slider
-  new Swiper('.reviews-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
     // Shrink the navbar 
     navbarShrink();
 
